a0467802eec49b337bce5ba2b4de2c320d271d9d
prayag <prayag@prayag.(none)>
2013-08-11 16:19:32 +0545

    REMOVE unwanted files

--- QuikProject/src/quik/pro/Main.java
+++
@@ -1,9 +0,0 @@
-package quik.pro;
-
-public class Main {
-	public static void main(String[] ar){
-		System.out.print("Prayag Lerans Github.");
-		System.out.println("Why?.");
-		System.out.println("It's Good.");
-	}
-}
